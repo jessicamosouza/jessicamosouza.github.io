eea51c7f0bf34357a5f5654dacba8fd5884ac5e9
Jéssica Souza <jessica+pgp-signed-commit@souza.cloud>
2023-07-18 18:08:17 -0300

    First empty commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <Profile msg="Jéssica Souza" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Profile from "./components/Profile.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Profile,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+template {
+    height: 100vh;
+}
+</style>
